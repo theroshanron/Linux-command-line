07bdf00bb2af7d2cc6696ddc69af3691425ef05c
theroshanron <95roshan1@gmail.com>
2021-12-25 23:23:53 +0530

    Operators

--- notes.sh
+++ notes.sh
@@ -62,6 +62,20 @@ else
     
 fi #ends the if statement
 
-#ELSE_IF
+num1=3
+num2=5
+
+if [ "$num1" -gt "$num2" ]
+then
+    echo "num1 is greater than num2"
+else
+    echo "num2 is greater than num1"
+    
+fi
+
+#gt is used for greater than
+#lt is used for less than
+
+#File conditions
 
 
